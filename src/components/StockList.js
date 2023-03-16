c6d39098569e2bfbfd3da9635955c928d6ca73e4
Giovanni Barros <barrosgiovanni@icloud.com>
2023-03-16 13:38:26 -0300

    fetched data from finnhub api and started rendering on jsx

--- src/components/StockList.js
+++ src/components/StockList.js
@@ -4,25 +4,84 @@ import finnHub from '../apis/finnHub';
 
 function StockList() {
 
-  const [ watchList, setWatchList ] = useState(['GOOGL', 'MSFT', 'AMZN', 'APPL']);
+  const [stocks, setStocks] = useState([]);
+
+  const [ watchList, setWatchList ] = useState(['GOOGL', 'MSFT', 'AMZN']);
 
   useEffect(() => {
 
+    let isMounted = true;
+
     const fetchData = async () => {
+
       try {
-        const response = await finnHub.get('/quote?symbol=MSFT');
-        console.log(response);
+
+        const responses = await Promise.all(watchList.map((symbol) => {
+
+          return finnHub.get('/quote', {
+            params: {
+              symbol: symbol
+            }
+          });
+        }));
+
+        const updatedResponses = responses.map((response) => {
+          const symbol = response.config.params.symbol;
+          const data = response.data;
+          return {...data, symbol};
+        })
+
+        if (isMounted) {
+          setStocks(updatedResponses);
+        }
+
       } catch (error) {
 
       }
+
     }
 
     fetchData();
 
-  }, [])
+    return () => isMounted = false;
+
+  }, []);
+
+  const renderStocks = stocks.map((stock) => {
+    return (
+        <tr className='table-row' key={stock.symbol}>
+          <th scope='row'>{stock.symbol}</th>
+          <td>{stock.c}</td>
+          {stock.d > 0 ? <td className='text-success'>{stock.d}</td> : <td className='text-danger'>{stock.d}</td>}
+          {stock.d > 0 ? <td className='text-success'>{stock.d}</td> : <td className='text-danger'>{stock.dp}%</td>}
+          <td>{stock.h}</td>
+          <td>{stock.l}</td>
+          <td>{stock.o}</td>
+          <td>{stock.pc}</td>
+        </tr>
+    )
+  });
 
   return (
-    <div>StockList</div>
+    <div>
+      <table className='table hover mt-5'>
+        <thead className='table-head'>
+          <tr>
+            <th scope='col'>Symbol</th>
+            <th scope='col'>Price</th>
+            <th scope='col'>Change</th>
+            <th scope='col'>Change %</th>
+            <th scope='col'>High</th>
+            <th scope='col'>Low</th>
+            <th scope='col'>Open</th>
+            <th scope='col'>Close</th>
+          </tr>
+        </thead>
+        <tbody>
+          {renderStocks}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
