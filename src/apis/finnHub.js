0f8f58f04c19255f59af36607ffc83db6d48b85e
Giovanni Barros <barrosgiovanni@icloud.com>
2023-03-20 11:19:01 -0300

    created .evn file to protect api requests

--- src/apis/finnHub.js
+++ src/apis/finnHub.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-const apiKey = 'cg8b5m9r01qgf5t8tkvgcg8b5m9r01qgf5t8tl00';
-
 export default axios.create({
   baseURL: 'https://finnhub.io/api/v1',
   params: {
-    token: apiKey
+    token: process.env.REACT_APP_FINNHUB_API_KEY
   }
 })
