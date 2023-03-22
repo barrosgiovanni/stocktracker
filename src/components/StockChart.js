e88f8bbeb7ce612d0da0d793dc3beec6d1fc8547
Giovanni Barros <barrosgiovanni@icloud.com>
2023-03-22 11:50:51 -0300

    Configured apex chart with minimal setup

--- src/components/StockChart.js
+++ src/components/StockChart.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from 'react';
+import Chart from "react-apexcharts";
 
-function StockChart({ chartData }) {
+function StockChart({ chartData, symbol }) {
+
+  const { day, week, month, year } = chartData;
+
+  const options = {
+    title: {
+      text: symbol,
+      align: 'center',
+      style: {
+        fontSize: '28px'
+      }
+    },
+    chart: {
+      id: 'stock data',
+      animations: {
+        speed: 1200
+      }
+    },
+    xaxis: {
+      type: 'datetime',
+      labels: {
+        datetimeUTC: false
+      }
+    },
+    tooltip: {
+      x: {
+        format: 'MMM dd HH:MM'
+      }
+    }
+  }
+
+  const series = [{
+    name: symbol,
+    data: day
+  }]
 
   return (
-    <h2>stockChart</h2>
+    <div className='chart-display mt-5 p-4 shadow-sm'>
+      <Chart
+        options={options}
+        series={series}
+        type='area'
+        width='100%' />
+      <div className='interval-buttons mt-3'>
+        <button className='btn-interval'>Day</button>
+        <button className='btn-interval'>Week</button>
+        <button className='btn-interval'>Month</button>
+        <button className='btn-interval'>Year</button>
+      </div>
+    </div>
   )
 }
 
