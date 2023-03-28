d051489781644cb4735214e322f078543715d8e6
Giovanni Barros <barrosgiovanni@icloud.com>
2023-03-28 13:30:42 -0300

    deploying through gh-pages

--- src/components/StockDetails.js
+++ src/components/StockDetails.js
@@ -13,8 +13,7 @@ function StockDetails({ symbol}) {
     exchange,
     ipo,
     marketCapitalization,
-    shareOutstanding,
-    weburl
+    shareOutstanding
   } = stockData;
 
   useEffect(() => {
@@ -89,18 +88,3 @@ function StockDetails({ symbol}) {
 }
 
 export default StockDetails;
-
-// {
-//   "country": "US",
-//   "currency": "USD",
-//   "exchange": "NASDAQ/NMS (GLOBAL MARKET)",
-//   "ipo": "1980-12-12",
-//   "marketCapitalization": 1415993,
-//   "name": "Apple Inc",
-//   "phone": "14089961010",
-//   "shareOutstanding": 4375.47998046875,
-//   "ticker": "AAPL",
-//   "weburl": "https://www.apple.com/",
-//   "logo": "https://static.finnhub.io/logo/87cb30d8-80df-11ea-8951-00000000092a.png",
-//   "finnhubIndustry":"Technology"
-// }
